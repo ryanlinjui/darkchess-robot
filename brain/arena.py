f304e619777d5c9f3a2d4c8b4e718ca22da29c87
ryanlinjui <ryanlinjui@gmail.com>
2022-07-19 14:08:29 +0800

    feat: darkchess arena env

---
+++ brain/arena.py
@@ -0,0 +1,111 @@
+# -*- coding: utf-8 -*-
+import random
+import os
+from .utils import available
+from globfile import (
+    ALL_CHESS,
+    CHESS_ENG2TW,
+    EN_CHESS
+)
+
+class battle:
+    def __init__(self,
+                 first_player,
+                 secondary_player,
+                 ):
+        self.first_player = first_player
+        self.secondary_player = secondary_player
+        self.board = None
+        self.r_board = None
+        self.color = None
+        self.availablestep = None
+        self.turn = None
+        self.drawstep = None
+        self.first_player_name = first_player.__qualname__.replace(".action","") + "(Player 1)"
+        self.secondary_player_name = secondary_player.__qualname__.replace(".action","") + "(Player 2)"
+        self.color_name = {1:None,
+                           -1:None}
+
+    def initial_game(self):
+        self.board = [EN_CHESS[14]] * 32
+        self.drawstep = 0
+        self.r_board = ALL_CHESS.copy()
+        random.shuffle(self.r_board)
+        self.color = 0
+        self.turn = 1
+
+    def end_game(self):
+        if len(self.availablestep) == 0:
+            if self.color == 1:
+                print(self.color_name[-1]+" RED WIN!!")
+            else:
+                print(self.color_name[1]+" BLACK WIN!!")
+            return True
+        elif self.drawstep >=50:
+            print("DRAW")
+            return True
+        return False
+
+    def set_color(self):
+        if self.board.count(EN_CHESS[14]) == 31:
+            for i in range(32):
+                if self.board[i] != EN_CHESS[14]:
+                    if ALL_CHESS.index(self.board[i])<16:
+                        self.color = 1
+                    else:
+                        self.color = -1
+                    self.color_name[self.color] = self.first_player_name
+                    self.color_name[self.color*-1] = self.secondary_player_name
+        self.color *= -1
+    
+    def create_availablestep(self):
+        self.availablestep = available(self.board,self.color)
+
+    def show_board(self):
+        #os.system("cls")
+        show = ""
+        for i in range(32):
+            show += CHESS_ENG2TW[self.board[i]]
+            if (i+1)%8==0:
+                print(show)
+                show = ""
+        print("Drawstep:"+str(self.drawstep))
+
+    def board_update(self):
+        while True:
+            if self.color == 1:
+                print("BLACK")
+            elif self.color == -1:
+                print("RED")
+            if self.turn == 1:
+                action = self.first_player(self.board,self.color)
+            elif self.turn == -1:
+                action = self.secondary_player(self.board,self.color)
+
+            if action in self.availablestep:
+                if self.board[action[1]] == EN_CHESS[15]:
+                    self.drawstep += 1
+                else:
+                     self.drawstep = 0
+                if action[0] == action[1]:  #open
+                    self.board[action[0]] = self.r_board[action[0]]
+                else:
+                    self.board[action[1]] = self.board[action[0]]
+                    self.board[action[0]] = EN_CHESS[15]
+                self.turn *= -1
+                break
+            else:
+                print("invalid action!!")
+
+def loop_game(first_player,secondary_player):
+    bt = battle(first_player,secondary_player)
+    bt.initial_game()
+    bt.show_board()
+    while True:
+        bt.set_color()
+        bt.create_availablestep()      
+        if bt.end_game():
+            break
+        bt.board_update()
+        bt.show_board()
+        
