f14b1d0cb2cbd0c7eb430c4cfa61801d18062f45
ryanlinjui <ryanlinjui@gmail.com>
2023-08-30 16:01:34 +0800

    refactor: app

--- app.py
+++ app.py
@@ -1,43 +1,68 @@
-from flask import Flask,request,jsonify
+from flask import Flask, request, jsonify, make_response
+import cv2
+import numpy as np
+
 from eye import*
 from brain import*
 
 app = Flask(__name__)
 
-@app.route("/brain/<agent>",methods=["GET"])
-def brain_api(agent):
-    board = list(request.args.get("board"))
-    # eaten = request.args.get("eaten") TODO: eaten chess judge check
-    data = {
-        "black" : [-1,-1],
-        "red" : [-1,-1]
-    }
+brain_route = {
+    "ramdom": Random(),
+    "min-max": Min_Max(4),
+    "alpha-beta": Alpha_Beta(4)
+}
 
+@app.route("/brain", methods=["POST"])
+def brain():
     try:
-        if agent == "random":
-            data["black"] = Random().action(board,1)
-            data["red"] = Random().action(board,-1)
+        data = request.json
+        if data["algorithm"] in brain_route.keys():
+            return make_response(jsonify(
+                {
+                    "black": brain_route[data["algorithm"]].action(list(data["board"]), 1),
+                    "red": brain_route[data["algorithm"]].action(list(data["board"]), -1)
+                }
+            ), 200)
 
-        elif agent == "minmax":
-            data["black"] = Min_Max(2).action(board,1)
-            data["red"] = Min_Max(2).action(board,-1)
+    except Exception as e:
+        print(e)
 
-        elif agent == "alphabeta":
-            data["black"] = Alpha_Beta(2).action(board,1)
-            data["red"] = Alpha_Beta(2).action(board,-1)
-        return jsonify(data)
-    except:
-        return "Error: Status 503"
+    return make_response(jsonify(
+        {
+            "error": "brain data issue occur"
+        }
+    ), 400)
         
-@app.route("/eye/<mode>",methods=["GET"])
-def eye_api(mode):
-    # url = "http://172.20.10.1:8081/video"
-    url = request.args.get("url")
-    if mode == "single":
-        return single_chess(url=url)
-    elif mode == "full":
-        return full_board(url=url)
-    return "Error: Status 503"
+@app.route("/eye", methods=["POST"])
+def eye():
+    try:
+        img = cv2.imdecode(np.frombuffer(request.data, np.uint8), cv2.IMREAD_COLOR)
+        return make_response(full_board(img), 200)
+
+    except Exception as e:
+        print(e)
+        
+    return make_response(jsonify(
+        {
+            "error": "image data error"
+        }
+    ), 400)
+
+@app.route("/eye/url", methods=["POST"])
+def eye_url():
+    try:
+        img = cv2.VideoCapture(request.json["url"]).read()[1]
+        return make_response(full_board(img), 200)
+
+    except Exception as e:
+        print(e)
+        
+    return make_response(jsonify(
+        {
+            "error": "url of image error"
+        }
+    ), 400)
 
 if __name__ == "__main__":
-    app.run(debug=True,host="0.0.0.0", port=814,threaded=False)
+    app.run(debug=True, host="0.0.0.0", port=8080, threaded=False)
